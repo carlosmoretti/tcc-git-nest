b7dffee841cac9326cbe522e411584a9d46bdb3a
Carlos Alexandre <37070689+carlosmoretti@users.noreply.github.com>
2021-09-22 20:05:30 -0300

    gerando senha criptografada

--- src/service/responsavel/responsavel.service.ts
+++ src/service/responsavel/responsavel.service.ts
@@ -1,9 +1,9 @@
+import { Responsavel } from './../../model/responsavel.model';
 import { MensagensConst } from './../../mensagem.const';
 import { jwtConstants } from './../../constants';
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Responsavel } from 'src/model/responsavel.model';
 import { Repository } from 'typeorm/repository/Repository';
 import * as bcrypt from 'bcrypt';
 import { ServiceBase } from '../service';
@@ -23,5 +23,10 @@ export class ResponsavelService extends ServiceBase<Responsavel> {
         resp.senha = await bcrypt.hash(resp.senha, jwtConstants.bcrypt_salts);
         await this.whatsappService.enviar("+5521969416765", MensagensConst.confirmacaoCriacaoContaWhatsApp);
         super.create(resp);
+    }
+
+    async update(obj: Responsavel) {
+        obj.senha = await bcrypt.hash(obj.senha, jwtConstants.bcrypt_salts);
+        super.update(obj);
     }
 }
